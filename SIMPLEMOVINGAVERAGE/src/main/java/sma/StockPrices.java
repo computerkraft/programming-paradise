2ac9a12710d70d2ca62f6c91487d87caf9c94d58
SDON <computerkraft@users.noreply.github.com>
2016-03-13 17:36:53 -0700

    SIMPLEMOVINGAVERAGE
    Map Reduce application that computes the simple moving average of a stock traded on the NYSE.
    Input file: NYSE_daily_prices_J.csv

---
+++ SIMPLEMOVINGAVERAGE/src/main/java/sma/StockPrices.java
@@ -0,0 +1,86 @@
+package sma;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+class StockPrices implements Writable {
+	
+	private double open, high, low, close, adjclose;
+	private int volume;
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		
+		out.writeDouble(open);
+		out.writeDouble(high);
+		out.writeDouble(low);
+		out.writeDouble(close);
+		out.writeDouble(adjclose);
+		out.writeInt(volume);
+
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		
+		open = in.readDouble();
+		high = in.readDouble();
+		low = in.readDouble();
+		close = in.readDouble();
+		adjclose = in.readDouble();	
+		volume = in.readInt();
+
+	}
+
+	public double getOpen() {
+		return open;
+	}
+
+	public void setOpen(double open) {
+		this.open = open;
+	}
+
+	public double getLow() {
+		return low;
+	}
+
+	public void setLow(double low) {
+		this.low = low;
+	}
+
+	public double getClose() {
+		return close;
+	}
+
+	public void setClose(double close) {
+		this.close = close;
+	}
+
+	public double getAdjclose() {
+		return adjclose;
+	}
+
+	public void setAdjclose(double adjclose) {
+		this.adjclose = adjclose;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public double getHigh() {
+		return high;
+	}
+
+	public void setHigh(double high) {
+		this.high = high;
+	}
+
+}
